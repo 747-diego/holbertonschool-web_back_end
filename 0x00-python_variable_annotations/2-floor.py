7b1237069b9a4229aacd94d416045164e5d1d0ff
747-diego <725@holbertonschool.com>
2021-03-15 09:06:22 -0400

    [TASK 3. Basic annotations - to string]

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Write a type-annotated function floor."""
+
+
+def floor(n: float) -> int:
+    """A-function floor which takes a float as argument."""
+    """And returns the floor of the float."""
+    return(int(n))
