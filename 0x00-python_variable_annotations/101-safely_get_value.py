f8f6cbeea2973cd4ffb48a15aedb352f0f6cc8bc
747-diego <725@holbertonschool.com>
2021-03-15 23:51:40 -0400

    [TASK 11]

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Write a type-annotated function safe_get_value."""
 
-from typing import Dict
+import typing
 
 
-def safely_get_value(dct, key, default=None):
+def safely_get_value(dct: typing.Mapping, key: typing.Any, default=None):
     """Given the above parameters and the return values."""
     """Add type annotations to the function safely_get_value."""
     if key in dct:
