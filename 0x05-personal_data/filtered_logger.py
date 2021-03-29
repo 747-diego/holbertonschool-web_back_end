d0c5d459b5e3605700e37cc79700a24b5ce45329
747-diego <725@holbertonschool.com>
2021-03-29 16:46:58 -0400

    [TASK 1. Log formatter]

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -7,10 +7,34 @@ import os
 import re
 
 
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class."""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: typing.List[str]):
+        """Redacting-Fields."""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Logging-the-Occurences."""
+        loggedOccurences = logging.Formatter(self.FORMAT).format(record)
+        fields = self.fields
+        red = self.REDACTION
+        sep = self.SEPARATOR
+        return(filter_datum(fields, red, loggedOccurences, sep))
+
+
 def filter_datum(fields: typing.List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Return the log message obfuscated."""
     for obfuscated in fields:
         message = re.sub(obfuscated + "=.*?" + separator,
                          obfuscated + "=" + redaction + separator, message)
-    return message
+    return(message)
