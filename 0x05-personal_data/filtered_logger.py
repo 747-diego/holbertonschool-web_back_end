294676d3e2b234c125e2a1618eb7f469e92bb6a2
747-diego <725@holbertonschool.com>
2021-03-29 13:30:26 -0400

    [TASK 0. Regex-ing]

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -4,13 +4,13 @@
 import typing
 import logging
 import os
-from re import sub
+import re
 
 
 def filter_datum(fields: typing.List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Return the log message obfuscated."""
     for obfuscated in fields:
-        message = sub(obfuscated + "=.*?" + separator,
-                      obfuscated + "=" + redaction + separator, message)
+        message = re.sub(obfuscated + "=.*?" + separator,
+                         obfuscated + "=" + redaction + separator, message)
     return message
