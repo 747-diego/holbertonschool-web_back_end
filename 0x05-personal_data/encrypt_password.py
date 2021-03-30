6bce41c78b878f8ef2b38ccfc56d8aa4fef971bf
747-diego <725@holbertonschool.com>
2021-03-30 00:07:52 -0400

    [TASK 6. Check valid password]

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -6,16 +6,16 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """A-salted, hashed password, which is a byte string."""
     UTF = 'utf8'
-    PWencode = password.encode()
+    PWencode = password.encode(UTF)
     SaltyString = bcrypt.gensalt()
-    password = bcrypt.hashpw(PWencode(UTF), SaltyString)
+    password = bcrypt.hashpw(PWencode, SaltyString)
     return(password)
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """Password Validation."""
     UTF = 'utf8'
-    PWencode = password.encode()
+    PWencode = password.encode(UTF)
     ValidatedHash = hashed_password
-    password = bcrypt.checkpw(PWencode(UTF), ValidatedHash)
+    password = bcrypt.checkpw(PWencode, ValidatedHash)
     return(password)
