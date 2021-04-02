1b2e3b1c12e8aae93a7505e0eabe0deb085d0a7a
747-diego <725@holbertonschool.com>
2021-04-01 23:11:01 -0400

    [TASK 3. Auth class ]

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Auth class."""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """For all authentication system."""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Public method."""
+        if len(excluded_paths) == 0:
+            return(True)
+        if path is None:
+            return(True)
+        if excluded_paths is None:
+            return(True)
+        if path[-1] != '/':
+            path = path + '/'
+        if path in excluded_paths:
+            return(False)
+        return(True)
+
+    def authorization_header(self, request=None) -> str:
+        """Public method."""
+        if "Authorization" not in request.headers:
+            return(None)
+        if request is None:
+            return(None)
+        else:
+            return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Public method."""
+        return(None)
