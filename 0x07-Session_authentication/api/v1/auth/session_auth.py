65da772da52ff8d8d795504b6ba05d758843224b
747-diego <725@holbertonschool.com>
2021-04-06 18:55:59 -0400

    [GENERATED SESSIONS]

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -8,3 +8,15 @@ import uuid
 
 class SessionAuth(Auth):
     """Session Authentication."""
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Instance method that creates a Session ID."""
+        if user_id is None:
+            return(None)
+        if user_id is not str:
+            return(None)
+        GeneratedSession = str(uuid.uuid4())
+        self.user_id_by_session_id[GeneratedSession] = user_id
+        return(GeneratedSession)
