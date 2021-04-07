744c1387ecd58dc5c537494ba6f1fc8c359ff3c1
747-diego <725@holbertonschool.com>
2021-04-07 15:45:27 -0400

    [TESTING]

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -34,5 +34,7 @@ class SessionAuth(Auth):
         """User instance based on a cookie value."""
         SessionCookie = self.session_cookie(request)
         CookieID = self.user_id_for_session_id(SessionCookie)
+        if type(CookieID) == dict:
+            CookieID = CookieID["user_id"]
         UserID = User.get(CookieID)
         return(UserID)
