ff4d49f56ab810d696aa496e3f3742ddf764e4ce
747-diego <725@holbertonschool.com>
2021-04-19 18:31:25 -0400

    [TASK 1. Creating User]

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -29,8 +29,8 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Save the user to the database."""
-        # Adding user to the database
+        """Save the user to the Database."""
+        # Adding user to the Database
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
