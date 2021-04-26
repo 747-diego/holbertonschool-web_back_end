17af6735881a3725d9e50f7ece1a346882c43afe
747-diego <725@holbertonschool.com>
2021-04-25 23:36:20 -0400

    [EXTRA POINTS]

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -4,7 +4,8 @@
 from unittest import mock
 import unittest
 from unittest.mock import patch, Mock
-from parameterized import parameterized
+from parameterized import parameterized, parameterized_class
+import fixtures
 from client import GithubOrgClient
 from utils import get_json
 
@@ -24,3 +25,42 @@ class TestGithubOrgClient(unittest.TestCase):
         GithubUser = GithubOrgClient(client)
         GithubUser.org()
         api.assert_called_once_with("https://api.github.com/orgs/"+client)
+
+    def test_public_repos_url(self):
+        """Making a property."""
+        pass
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock):
+        """More patching."""
+        pass
+
+    def test_has_license(self, repo, license, expected):
+        """Parameterize."""
+        self.assertEqual(GithubOrgClient.has_license(repo, license), expected)
+
+
+@parameterized_class(
+    ('org_payload', 'repos_payload', 'expected_repos', 'apache2_repos'),
+    fixtures.TEST_PAYLOAD
+)
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """Integration test: fixtures."""
+
+    @classmethod
+    def setUpClass(test):
+        """Unit-test case API."""
+        pass
+
+    @classmethod
+    def tearDownClass(test):
+        """Unit-test case API."""
+        pass
+
+    def test_public_repos(self):
+        """Unit-test case API."""
+        pass
+
+    def test_public_repos_with_license(self):
+        """Unit-test case API."""
+        pass
