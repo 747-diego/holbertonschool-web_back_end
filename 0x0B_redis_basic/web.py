c702b9ae26412443eb5f02124d97ec28d3ff70b5
747-diego <725@holbertonschool.com>
2021-04-27 23:26:49 -0400

    [CACHING]

---
+++ 0x0B_redis_basic/web.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""G e t  a  p a g e  a n d  c o u n t  t i m e s  a c c e s s e d"""
+
+from typing import Union, Callable
+from functools import wraps
+import redis
+import requests
+
+red = redis.Redis()
+red.flushdb()
+
+
+def my_cache(method: Callable) -> Callable:
+    """G e t  a  p a g e  a n d  c o u n t  t i m e s  a c c e s s e d"""
+    @wraps(method)
+    def wrap(*args, **kwargs):
+        """G e t  a  p a g e  a n d  c o u n t  t i m e s  a c c e s s e d"""
+        url = args[0]
+        text = red.get(url)
+        if text is None:
+            text = method(*args, **kwargs)
+            if text is not None:
+                red.setex(url, 10, text)
+                red.incr("count:"+url)
+        else:
+            text = text
+            red.incr("count:"+url)
+        return text
+    return wrap
+
+
+@my_cache
+def get_url(url: str) -> bytes:
+    """G e t  a  p a g e  a n d  c o u n t  t i m e s  a c c e s s e d"""
+    try:
+        text = requests.get(url).content
+        return text
+    except Exception:
+        return None
+
+
+def get_page(url: str) -> str:
+    """G e t  a  p a g e  a n d  c o u n t  t i m e s  a c c e s s e d"""
+    return get_url(url)
