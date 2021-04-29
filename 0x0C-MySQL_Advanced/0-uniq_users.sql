2f6464f6b8ac1fee1c7d7fc569b09e3f6f07b5f2
747-diego <725@holbertonschool.com>
2021-04-29 10:53:51 -0400

    [TASK 0. We are all unique]ls

---
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- We are all unique!
+
+CREATE TABLE IF NOT EXISTS users (
+       id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+       email varchar(255) NOT NULL UNIQUE,
+       name varchar(255)
+);
