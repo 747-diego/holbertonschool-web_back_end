0ab83128cf0264a4d4f9f76bd2cb5666b961dcd8
747-diego <725@holbertonschool.com>
2021-04-29 16:53:16 -0400

    [TASK 5. Email validation to sent]

--- 0x0C-MySQL_Advanced/5-valid_email.sql
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -1,11 +1,11 @@
 -- Email validation to sent
 -- SQL script that creates a trigger that resets the attribute valid_email
+DELIMITER $$
 CREATE TRIGGER 
 validATTR
 BEFORE UPDATE 
 ON users FOR EACH ROW
-BEGIN
-    IF OLD.email <> NEW.email THEN
-    SET NEW.valid_email = 0;
-    END IF;
-END$$
+IF OLD.email <> NEW.email THEN
+SET NEW.valid_email = 0;
+END IF;
+DELIMITER;
