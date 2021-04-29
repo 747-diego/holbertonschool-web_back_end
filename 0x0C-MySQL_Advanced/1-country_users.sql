247991089c65ecfdf404d1f87eb5568e79ba4d26
747-diego <725@holbertonschool.com>
2021-04-29 16:35:25 -0400

    [TASK 2. Best band ever]

--- 0x0C-MySQL_Advanced/1-country_users.sql
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,8 @@
 -- In and not out
 -- SQL script that creates a table
-
 CREATE TABLE IF NOT EXISTS users (
        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
        email varchar(255) NOT NULL UNIQUE,
-       name varchar(255),
+       NAME varchar(255),
        country ENUM ('US', 'CO', 'TN') NOT NULL
 );
