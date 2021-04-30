74a1addd0a3a2931e7bd28aa0af6ee11237be823
747-diego <725@holbertonschool.com>
2021-04-29 23:32:21 -0400

    [TASK 6. Add bonus]

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -1,20 +1,5 @@
 -- Add bonus
 -- SQL script that creates a stored procedure AddBonus
-DELIMITER $$
 CREATE PROCEDURE AddBonus 
 (IN user_id INT, project_name VARCHAR(255), 
 score INT)
-BEGIN
-	IF NOT EXISTS (
-	   SELECT *
-	   FROM projects
-	   WHERE NAME = project_name)
-	   THEN
-	   	INSERT INTO projects(NAME) VALUES(project_name);
-	END IF;
-	INSERT INTO studentError(user_id, score,  project_id)
-	       VALUES(user_id, score,
-			(SELECT id from projects
-			WHERE NAME = project_name));
-END$$
-DELIMITER;
