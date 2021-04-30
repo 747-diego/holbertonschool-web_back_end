a5a7a937c5bb157493e12f6200ec3b782c79895d
747-diego <725@holbertonschool.com>
2021-04-29 23:48:50 -0400

    [TASK 13. Average weighteed score for all ]

--- 0x0C-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x0C-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,4 +1,3 @@
 -- Average weighted score for all!
 -- SQL script that creates a stored procedure ComputeAverageWeightedScoreForUsers
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers
-(IN user_id INT)
