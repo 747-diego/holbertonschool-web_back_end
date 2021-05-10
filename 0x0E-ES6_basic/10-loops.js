9139e8552aae55ea2a0cc5867393b0b900474798
747-diego <725@holbertonschool.com>
2021-05-10 00:03:04 -0400

    [TEST]

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   const appendedArray = [];
-  for (const idx of appendedArray) {
+  for (const idx of array) {
     appendedArray.push(appendString + idx);
   }
   return appendedArray;
