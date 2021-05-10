b2aa12ba49c864e8559703483f05364ffffcf54b
747-diego <725@holbertonschool.com>
2021-05-09 23:44:37 -0400

    [TASK 12. lets create a report object]

--- 0x0E-ES6_basic/12-createReportObject.js
+++ 0x0E-ES6_basic/12-createReportObject.js
@@ -1,3 +1,9 @@
 export default function createReportObject(employeesList) {
-  return ({ allEmployees: { ...employeesList } });
+  const report = {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(employeesList) {
+      return (Object.keys({ ...employeesList }).length);
+    }
+  };
+  return (report);
 }
