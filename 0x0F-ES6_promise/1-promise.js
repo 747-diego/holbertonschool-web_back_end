74cac9bceec1b51517ff25a4a22f49fe922db482
747-diego <725@holbertonschool.com>
2021-05-11 17:35:04 -0400

    [TASK 1. Dont make a promise if you know you cant keep it]

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -2,9 +2,7 @@ export default function getFullResponseFromAPIj(success) {
   const APIresponse = new Promise((resolve, reject) => {
     if (success) {
       resolve(
-        {
-          status: 200,
-          body: 'Success' },
+        { status: 200, body: 'Success' },
       );
     } else {
       reject(new Error('The fake API is not working currently'));
