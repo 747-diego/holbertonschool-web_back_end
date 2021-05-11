ae94955f78d961462ece072913b1dcdec3869411
747-diego <725@holbertonschool.com>
2021-05-11 19:31:31 -0400

    [TASK 3.]

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,37 +1,15 @@
 import { uploadPhoto, createUser } from './utils';
 
-// export default function handleProfileSignup() {
-//   let firstName;
-//   let lastName;
-//   let body;
-
-//   return uploadPhoto()
-//     .then((data) => {
-//       body = data.body;
-//       createUser()
-//         .then((data) => {
-//           firstName = data.firstName;
-//           lastName = data.lastName;
-//           console.log(`${body} ${firstName} ${lastName}`);
-//         })
-//         .catch(() => console.log('Signup system offline'));
-//     })
-//     .catch(() => console.log('Signup system offline'));
-// }
-
 export default function handleProfileSignup() {
   const x = uploadPhoto();
   const y = createUser();
+  let body;
   let firstName;
   let lastName;
-  let body;
-
-  return Promise.all([x, y])
-    .then((values) => {
-      body = values[0].body;
-      firstName = values[1].firstName;
-      lastName = values[1].lastName;
-      console.log(`${body} ${firstName} ${lastName}`).catch(
-        () => console.log('Signup system offline'));
-    });
+  return Promise.all([x, y]).then((values) => {
+    firstName = values.firstName;
+    lastName = values.lastName;
+    body = values.body;
+    console.log(firstName, lastName, body);
+  }, () => console.log('Signup system offline'));
 }
