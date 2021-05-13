c04a10d465ffa6c8992d6a1dc871ab1d88577193
747-diego <725@holbertonschool.com>
2021-05-13 17:39:15 -0400

    [TASK 1. Lets make some classrooms]

--- 0x10-ES6_classes/1-make_classrooms.js
+++ 0x10-ES6_classes/1-make_classrooms.js
@@ -3,12 +3,12 @@ import ClassRoom from './0-classroom';
 export default function initializeRooms() {
   const classArray = [];
   //   const students = [19, 20, 34];
-  const classroomOne = 19;
-  const classroomTwo = 20;
-  const classroomThree = 34;
+  const classroomOne = [19];
+  const classroomTwo = [20];
+  const classroomThree = [34];
   const roomOne = classArray.push(new ClassRoom(classroomOne));
   const roomTwo = classArray.push(new ClassRoom(classroomTwo));
   const roomThree = classArray.push(new ClassRoom(classroomThree));
 
-  return ([roomOne, roomTwo, roomThree]);
+  return (roomOne, roomTwo, roomThree);
 }
