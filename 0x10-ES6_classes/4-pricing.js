973c3a0ea7eca0dce7398b535ed5fc567146239b
747-diego <725@holbertonschool.com>
2021-05-13 22:58:33 -0400

    [TASK 4. Pricing]

--- 0x10-ES6_classes/4-pricing.js
+++ 0x10-ES6_classes/4-pricing.js
@@ -1,36 +1,36 @@
-import Currency from './3-currency';
+import Currency from './3-currency'
 
 export default class Pricing {
-  constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
+  constructor (amount, currency) {
+    this._amount = amount
+    this._currency = currency
   }
 
-  displayFullPrice() {
-    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  displayFullPrice () {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`
   }
 
-  static convertPrice(amount, conversionRate) {
-    return (amount * conversionRate);
+  static convertPrice (amount, conversionRate) {
+    return (amount * conversionRate)
   }
 
-  get amount() {
-    const amount = this._amount;
-    return (amount);
+  get amount () {
+    const amount = this._amount
+    return (amount)
   }
 
-  set amount(amount) {
-    // const setAmount = this._amount = amount;
-    return (this._amount = amount);
+  set amount (amount) {
+    const setAmount = this._amount = amount
+    return (setAmount)
   }
 
-  get currency() {
-    const currency = this._currency;
-    return (currency);
+  get currency () {
+    const currency = this._currency
+    return (currency)
   }
 
-  set currency(currency) {
-    const setCurrency = this._currency = currency;
-    return (setCurrency);
+  set currency (currency) {
+    const setCurrency = this._currency = currency
+    return (setCurrency)
   }
 }
