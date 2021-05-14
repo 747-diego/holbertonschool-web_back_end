8b56a9e691d931f0604e6e43179903db4c743918
747-diego <725@holbertonschool.com>
2021-05-13 22:01:49 -0400

    [TASK 2. A Course, Getters, Setters]

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -6,17 +6,17 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
-    if (typeof name !== 'string') throw TypeError('Name must be a string');
+    if (typeof name !== 'string') throw (TypeError('Name must be a string'));
     this._name = name;
   }
 
   set length(length) {
-    if (typeof length !== 'number') throw TypeError('Length must be a number');
+    if (typeof length !== 'number') throw (TypeError('Length must be a number'));
     this._length = length;
   }
 
   set students(students) {
-    if ((students.length > 0 && typeof students[0] !== 'string') || !Array.isArray(students)) throw TypeError('Students must be an array');
+    if ((students.length > 0 && typeof students[0] !== 'string') || !Array.isArray(students)) throw (TypeError('Students must be an array'));
     this._students = students;
   }
 
