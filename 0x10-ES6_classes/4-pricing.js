47a6e295d7eae85baf5e440588f2655766d17716
747-diego <725@holbertonschool.com>
2021-05-13 22:28:25 -0400

    [TASK 4. Pricing]

---
+++ 0x10-ES6_classes/4-pricing.js
@@ -0,0 +1,39 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    const amount = this._amount;
+    const name = this._currency.name;
+    const code = this._currency.displayFullCurrency();
+    return (`${amount} ${name} (${code})`);
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return (amount * conversionRate);
+  }
+
+  set amount(amount) {
+    const setAmount = this._amount = amount;
+    return (setAmount);
+  }
+
+  get amount() {
+    const amount = this._amount;
+    return (amount);
+  }
+
+  set currency(currency) {
+    const setCurrency = this._currency = currency;
+    return (setCurrency);
+  }
+
+  get currency() {
+    const currency = this._currency;
+    return (currency);
+  }
+}
