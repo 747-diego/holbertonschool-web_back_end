e50740f479731379996da7155d2074d1c457bee9
747-diego <725@holbertonschool.com>
2021-05-18 23:46:52 -0400

    [TASK 10. More map data structures]

--- 0x11-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x11-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,9 @@
 export default function updateUniqueItems(uniqueItem) {
   if (uniqueItem instanceof Map) throw (TypeError('Cannot process'));
-  uniqueItem.forEach((entry, item) => {
-    if (entry === 1) {
-      uniqueItem.set(item, 100);
+  for (const mapItem of uniqueItem) {
+    if (mapItem[1] === 1) {
+      uniqueItem.set(mapItem[0], 100);
     }
-  });
+  }
   return (uniqueItem);
 }
