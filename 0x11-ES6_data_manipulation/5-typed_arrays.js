73a989bf32a9b3f9e10621993ecf8bb173d768e0
747-diego <725@holbertonschool.com>
2021-05-18 23:19:50 -0400

    [TASK 5. Types Arrays]

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
   if (position >= length) {
-    throw Error('Position outside range');
+    throw (Error('Position outside range'));
   }
-  const buffer = new ArrayBuffer(length);
-  const view = new DataView(buffer, 0, length);
-  view.setInt8(position, value);
-  return view;
+  const arrayBuffer = new ArrayBuffer(length);
+  const data = new DataView(arrayBuffer, 0, length);
+  data.setInt8(position, value);
+  return (data);
 }
