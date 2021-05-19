f2ecc68ec61aebc96fc39da92cf882077b4019c5
747-diego <725@holbertonschool.com>
2021-05-18 22:43:50 -0400

    [TASK 4. Combine]

---
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,6 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const getListStudents = students.filter((student) => student.location === city);
+  return getListStudents.map((student) => {
+    return ({ undefined, grade: newGrades.filter((specificCity) => specificCity.studentId === student.id) ? newGrades.filter((specificCity) => specificCity.studentId === student.id).grade : 'N/A' });
+  });
+}
