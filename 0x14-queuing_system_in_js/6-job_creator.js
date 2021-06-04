eb4b0e7131b33e48b38b66415bf379a89723cbca
747-diego <725@holbertonschool.com>
2021-06-04 00:11:56 -0400

    [TASK 6. Create the Job creator]

---
+++ 0x14-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,19 @@
+import kue from 'kue';
+
+const job = kue.createQueue();
+const contactInfo = {
+  phoneNumber: '2038325152',
+  message: 'contact number',
+};
+
+const jobListing = job.create('push_notification_code', contactInfo).save(
+  (errorMessage) => {
+    if (!errorMessage) console.log(`Notification job created: ${jobListing.id}`);
+  });
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+job.on('failed', () => {
+  console.log('Notification job failed');
+});
