cb9c1831cca83a5a8d2d3be633235f54e02b3158
747-diego <725@holbertonschool.com>
2021-06-05 23:11:27 -0400

    [TASK 8. Track progrss and errors with Kue: Create the Job Creator]

---
+++ 0x14-queuing_system_in_js/7-job_creator.js
@@ -0,0 +1,67 @@
+import kue from 'kue';
+
+const jobs = [
+  {
+    phoneNumber: '4153518780',
+    message: 'This is the code 1234 to verify your account'
+  },
+  {
+    phoneNumber: '4153518781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4153518743',
+    message: 'This is the code 4321 to verify your account'
+  },
+  {
+    phoneNumber: '4153538781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4153118782',
+    message: 'This is the code 4321 to verify your account'
+  },
+  {
+    phoneNumber: '4153718781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4159518782',
+    message: 'This is the code 4321 to verify your account'
+  },
+  {
+    phoneNumber: '4158718781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4153818782',
+    message: 'This is the code 4321 to verify your account'
+  },
+  {
+    phoneNumber: '4154318781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4151218782',
+    message: 'This is the code 4321 to verify your account'
+  }
+];
+
+const queue = kue.createQueue();
+for (const job of jobs) {
+  const create = queue.create('push_notification_code_2', job).save((error) => {
+    if (!error) console.log(`Notification job created: ${create.id}`);
+
+    create.on('complete', () => {
+      console.log(`Notification job ${create.id} completed`);
+    });
+
+    create.on('failed', (error) => {
+      console.log(`Notification job ${create.id} failed: ${error}`);
+    });
+
+    create.on('progress', (progress) => {
+      console.log(`Notification job ${create.id} ${progress}% complete`);
+    });
+  });
+}
