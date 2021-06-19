9a28c6e99f43713587926ba12ca36dd89e3edbeb
747-diego <725@holbertonschool.com>
2021-06-19 18:00:20 -0400

    [TEST]

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -12,7 +12,7 @@ AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def index() -> str:
+def welcome() -> str:
     """Welcome-to-Flask."""
     return jsonify({"message": "Bienvenue"})
 
