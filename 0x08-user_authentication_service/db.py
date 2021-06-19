751d1b0c0f6dc652761718939fc9005dfb0c7dd1
747-diego <725@holbertonschool.com>
2021-06-19 16:37:53 -0400

    [TESTING]

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base
 from user import User
@@ -40,3 +42,24 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Finding-the-User."""
+        arguments = [
+                    "id",
+                    "email",
+                    "hashed_password",
+                    "session_id",
+                    "reset_token"
+                    ]
+
+        for keyword in kwargs:
+            if keyword not in arguments:
+                raise(InvalidRequestError)
+        userFound = self._session.query(User).filter_by(**kwargs).first()
+        if userFound:
+            return(userFound)
+        if userFound is None:
+            raise(NoResultFound)
+        return(userFound)
+
