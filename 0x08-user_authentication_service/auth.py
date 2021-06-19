03a3a6b1285a666e4265b6b7adfc5bcfe50a4e76
747-diego <725@holbertonschool.com>
2021-06-19 17:20:29 -0400

    [TEST]

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -8,6 +8,13 @@ from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> str:
-    """ Hash a password """
+    """Turn string arguments and returns bytes."""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
