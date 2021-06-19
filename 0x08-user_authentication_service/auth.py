d7f335286152832ea567313f0b7fb302a5be022c
747-diego <725@holbertonschool.com>
2021-06-19 18:35:04 -0400

    [TESTING]

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -3,6 +3,7 @@
 
 
 import bcrypt
+from bcrypt import checkpw
 import uuid
 from db import DB
 from user import User
@@ -33,9 +34,9 @@ class Auth:
         """If email exists check pasword."""
         try:
             found_user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(
+            return (checkpw(
                 password.encode('utf-8'),
                 found_user.hashed_password
-                )
+                ))
         except NoResultFound:
             return False
