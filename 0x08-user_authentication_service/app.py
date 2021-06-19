a990595aaa960bebd674ab366c369c4cb55683ad
747-diego <725@holbertonschool.com>
2021-06-19 18:02:35 -0400

    [TESTING]

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -16,6 +16,18 @@ def welcome() -> str:
     """Welcome-to-Flask."""
     return jsonify({"message": "Bienvenue"})
 
+@app.route("/users", methods=['POST'], strict_slashes=False)
+def register_route() -> str:
+    """ get the register route
+    """
+    body = request.form
+
+    try:
+        user = AUTH.register_user(body['email'], body['password'])
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
