c8049f18581fbba046052a178644fe1fab19d0aa
747-diego <725@holbertonschool.com>
2021-06-19 19:01:51 -0400

    TESTING]

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -47,12 +47,11 @@ class Auth:
             return False
 
     def create_session(self, email: str) -> str:
-        """TEST-TESTING."""
+        """Get session ID."""
+        session_id = _generate_uuid()
         try:
             user = self._db.find_user_by(email=email)
-            session_id = _generate_uuid()
-            user.session_id = session_id
-            self._db._session.commit()
-            return session_id
+            self._db.update_user(user.id, session_id=session_id)
+            return (session_id)
         except NoResultFound:
-            return None
+            return (None)
