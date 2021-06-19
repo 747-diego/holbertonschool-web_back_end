71ee0806253c7e4892323d1bd1b33abfb2948312
747-diego <725@holbertonschool.com>
2021-06-19 18:33:18 -0400

    [TESTING]

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -20,3 +20,22 @@ class Auth:
     def __init__(self):
         """Init."""
         self._db = DB()
+
+    def sign_up(self, email: str, password: str) -> User:
+        """Sign up and create account."""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+
+    def check_password(self, email: str, password: str) -> bool:
+        """If email exists check pasword."""
+        try:
+            found_user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(
+                password.encode('utf-8'),
+                found_user.hashed_password
+                )
+        except NoResultFound:
+            return False
