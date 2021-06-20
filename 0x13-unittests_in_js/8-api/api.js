d7c15510f08106489be1aaabc9126ac09de6f048
747-diego <725@holbertonschool.com>
2021-06-20 15:03:53 -0400

    [TESTING API]

--- 0x13-unittests_in_js/8-api/api.js
+++ 0x13-unittests_in_js/8-api/api.js
@@ -1,16 +1,14 @@
 const express = require('express');
+
 const app = express();
-
 const port = 7865;
-const welcomeMessage = 'Welcome to the payment system';
-const APIconnected = 'API available on localhost port 7865';
 
-app.get('/', function(req, res) {
-  res.send(welcomeMessage);
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
 });
 
-app.listen(port, function() {
-  console.log(APIconnected);
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
 });
 
 module.exports = app;
