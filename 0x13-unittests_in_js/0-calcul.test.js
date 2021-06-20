cf084d6ca97bc69c9a1a18f590f411e5e4845d61
747-diego <725@holbertonschool.com>
2021-06-20 02:18:34 -0400

    [TESTING SUM]

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -4,11 +4,8 @@ const assert = require('assert');
 describe('calculateNumber', () => {
   it('returns rounded sum', () => {
     assert.strictEqual(functionTest(1, 3), 4);
-    assert.strictEqual(functionTest(1, 3.7), 4.7);
-    assert.strictEqual(functionTest(1.2, 3.7), 5.9);
-    assert.strictEqual(functionTest(1.5, 3.7), 52);
-  });
-  it('should throw error if NaN passed', function () {
-    assert.throws(() => functionTest(NaN, 3), '[Function: TypeError]');
+    assert.strictEqual(functionTest(1, 3.7), 5);
+    assert.strictEqual(functionTest(1.2, 3.7), 5);
+    assert.strictEqual(functionTest(1.5, 3.7), 6);
   });
 });
