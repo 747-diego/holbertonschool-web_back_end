09e89dc631d661885b859782a196687bba37182a
747-diego <725@holbertonschool.com>
2021-06-20 18:59:04 -0400

    [TESTING SCORE]

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -6,4 +6,4 @@ BEGIN
 SET @name=(SELECT name FROM users WHERE user_id=id);
 SET @avg=(SELECT AVG(score) FROM corrections WHERE corrections.user_id=user_id);
 UPDATE users SET average_score = @avg WHERE name = @name;
-END//
+END
