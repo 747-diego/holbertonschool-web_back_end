a9c3cf621db8ffe88aecb70e328f6a2303389021
747-diego <725@holbertonschool.com>
2021-06-20 18:55:33 -0400

    [ADDING BONUS]

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -3,11 +3,11 @@
 delimiter //
 CREATE PROCEDURE addBonus (user_id INT, project_name CHAR(30), score INT)
 BEGIN
-    SET @p_id=(SELECT id FROM projects WHERE project_name = name);
-    IF @p_id IS NULL THEN
+    SET @bonus=(SELECT id FROM projects WHERE project_name = name);
+    IF @bonus IS NULL THEN
         INSERT INTO projects (name) VALUES(project_name);
     END IF;
-    SET @p_id=(SELECT id FROM projects WHERE project_name = name);
-    INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, @p_id, score);
+    SET @bonus=(SELECT id FROM projects WHERE project_name = name);
+    INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, @bonus, score);
 END//
 delimiter ;
