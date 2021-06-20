c23c9cae63724bbb5d94ef34907f6ce6bee217df
747-diego <725@holbertonschool.com>
2021-06-20 18:59:32 -0400

    [TESTING SCORE]

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -1,9 +1,10 @@
 -- Average Score
 -- SQL script that creates a stored procedure 
-
+delimiter //
 CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
 SET @name=(SELECT name FROM users WHERE user_id=id);
 SET @avg=(SELECT AVG(score) FROM corrections WHERE corrections.user_id=user_id);
 UPDATE users SET average_score = @avg WHERE name = @name;
-END
+END//
+delimiter ;
