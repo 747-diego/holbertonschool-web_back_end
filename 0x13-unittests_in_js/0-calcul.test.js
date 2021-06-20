f9153e4567dbd1fa73f7ba9d99116ea698edf641
747-diego <725@holbertonschool.com>
2021-06-20 02:25:55 -0400

    [TESTING]

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -7,5 +7,8 @@ describe('calculateNumber', () => {
     assert.strictEqual(functionTest(1, 3.7), 5);
     assert.strictEqual(functionTest(1.2, 3.7), 5);
     assert.strictEqual(functionTest(1.5, 3.7), 6);
+    assert.strictEqual(functionTest(1.4, 4.5), 6);
+    assert.strictEqual(functionTest(1.4, 0), 1);
+    assert.strictEqual(functionTest(0, 1.4), 1);
   });
 });
