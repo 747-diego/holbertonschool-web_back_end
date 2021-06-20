ebeb3f4f1a4b6b5f063a4126f4d2f8fb838cf830
747-diego <725@holbertonschool.com>
2021-06-19 23:16:07 -0400

    [TESTING HTTP]

---
+++ 0x12-Node_JS_basic/4-http.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const welcome = 'Hello Holberton School!';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.end(welcome);
+});
+
+app.listen(port, '127.0.0.1');
+
+module.exports = app;
