f6fc69a3cd025ab999d8d19543f0e7f988879098
747-diego <725@holbertonschool.com>
2021-06-19 22:25:33 -0400

    [STUDENTS]

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -10,8 +10,10 @@ function countStudents (file) {
   } catch (error) {
     throw (new Error('Cannot load the database'));
   }
+
   const names = name.split('\n');
   const students = names.slice(1).map((student) => student.split(',')).filter((student) => student.length > 0 && student[0] !== '');
+
   for (const student of students) {
     seats += 1;
     if (!(student[3] in classroom)) {
@@ -19,6 +21,7 @@ function countStudents (file) {
     }
     classroom[student[3]].push(student[0]);
   }
+
   console.log(`Number of students: ${seats}`);
   for (const num of Object.keys(classroom)) {
     console.log(`Number of students in ${num}: ${classroom[num].length}. List: ${classroom[num].join(', ')}`);
