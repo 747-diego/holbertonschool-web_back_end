b221d9f39c4e5d9f2d1fbcee0d9e0607d35f0356
747-diego <725@holbertonschool.com>
2021-06-20 17:15:15 -0400

    [TESTING API]

--- 0x13-unittests_in_js/9-api/api.test.js
+++ 0x13-unittests_in_js/9-api/api.test.js
@@ -1,14 +1,46 @@
-const request = require('request');
-const chai = require('chai')
 const { expect } = require('chai');
+const request = require('request');
+const { spy, stub } = require('sinon');
 
-describe('API test', () => {
-  it('respond with 200 code and body', (done) => {
-    request('http://localhost:7865', 'GET', (error, res, body) => {
-      if (error) throw error;
+
+describe('index page', () => {
+  beforeEach(() => {
+    this.get = stub(request, 'get');
+    this.post = stub(request, 'post');
+  });
+
+  afterEach(() => {
+    request.get.restore();
+    request.post.restore();
+
+  });
+
+  it('status code', () => {
+    request('http://localhost:7865/cart/12', function(err, res, body) {
       expect(res.statusCode).to.equal(200);
+    });
+  });
+  it('status code', () => {
+    request('http://localhost:7865/cart/hello', function(err, res, body) {
+      expect(res.statusCode).to.equal(404);
+    });
+  });
+  it('body returns 1', () => {
+    request('http://localhost:7865/', function(err, res, body) {
       expect(body).to.equal('Welcome to the payment system');
     });
-    done();
+  });
+
+
+  it('body returns 2', () => {
+    request('http://localhost:7865/cart/12', function(err, res, body) {
+      expect(body).to.equal('Payment methods for cart 12');
+    });
+  });
+
+  it('get?', () => {
+    request('http://localhost:7865/', function(err, res, body) {
+      expect(res.request.method).to.equal('GET');
+    });
   });
 });
