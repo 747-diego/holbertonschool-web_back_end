8d68e4b225d0e89f5cd105da26d39cc927b75dff
747-diego <725@holbertonschool.com>
2021-06-20 19:04:34 -0400

    [TESTING OPTIMIZATION]

--- 0x0C-MySQL_Advanced/8-index_my_names.sql
+++ 0x0C-MySQL_Advanced/8-index_my_names.sql
@@ -1,4 +1,3 @@
 -- Optimize simple search
 -- SQL script that creates an index idx_name_first on the table
-CREATE TABLE idx_name_first
-(IN user_id INT)
+CREATE INDEX idx_name_first ON names (name(1));
