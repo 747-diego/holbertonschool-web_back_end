cdda2c6bbee8a9ec412a9cb8d5ffa2eaef0a688e
747-diego <725@holbertonschool.com>
2021-06-20 16:28:18 -0400

    [TESTING API CONNECTION]

--- 0x13-unittests_in_js/8-api/api.test.js
+++ 0x13-unittests_in_js/8-api/api.test.js
@@ -5,7 +5,7 @@ const { expect } = require('chai');
 describe('API test', () => {
   it('respond with 200 code and body', (done) => {
     request('http://localhost:7865', 'GET', (err, res, body) => {
-      if (err) throw (err);
+      if (err) throw err;
       expect(res.statusCode).to.equal(200);
       expect(body).to.equal('Welcome to the payment system');
     });
