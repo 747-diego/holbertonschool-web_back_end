b8ec96fe7c909fee17583be6cfdfc09a5a954fc7
747-diego <725@holbertonschool.com>
2021-06-20 16:53:09 -0400

    [CONSOLE LOG MESSAGE]

--- 0x13-unittests_in_js/8-api/api.js
+++ 0x13-unittests_in_js/8-api/api.js
@@ -4,7 +4,7 @@ const app = express();
 const connection = 7865;
 
 app.get('/', (req, res) => {
-  console.log('Welcome to the payment system');
+  res.send('Welcome to the payment system');
 });
 
 app.listen(connection, () => {
