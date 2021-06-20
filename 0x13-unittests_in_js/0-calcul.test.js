e48dfe3b0dc35d10427edb20c7dc8a607d9b07f8
747-diego <725@holbertonschool.com>
2021-06-20 02:06:16 -0400

    [TESTING MOCHA]

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,16 +1,14 @@
-const functionTest = require("./0-calcul.js");
-const mocha = require('mocha');
-const assert = require("assert");
+const functionTest = require('./0-calcul.js');
+const assert = require('assert');
 
 describe('calculateNumber', () => {
-    it('returns rounded sum', () => {
+  it('returns rounded sum', () => {
     assert.strictEqual(functionTest(1, 3), 4);
-    assert.strictEqual(functionTest(1.6, 3), 5);
-    assert.strictEqual(functionTest(1.2, 3.8), 5);
-    assert.strictEqual(functionTest(-1, -3), -4);
-    assert.strictEqual(functionTest(-1.4, -3.6), -5);
-    });
-    it('should throw error if NaN passed', function () {
-        assert.throws(() => functionTest(NaN, 3), '[Function: TypeError]');
-    });
+    assert.strictEqual(functionTest(1, 3.7), 4.7);
+    assert.strictEqual(functionTest(1.2, 3.7), 5.9);
+    assert.strictEqual(functionTest(1.5, 3.7), 52);
+  });
+  it('should throw error if NaN passed', function () {
+    assert.throws(() => functionTest(NaN, 3), '[Function: TypeError]');
+  });
 });
