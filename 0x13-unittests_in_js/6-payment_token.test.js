2e89889dc2f9d1964d0ca45312fbbb6072a82e00
747-diego <725@holbertonschool.com>
2021-06-20 13:10:52 -0400

    [TESTING PAYMENT]

--- 0x13-unittests_in_js/6-payment_token.test.js
+++ 0x13-unittests_in_js/6-payment_token.test.js
@@ -1,13 +1,13 @@
-const { expect } = require('chai');
-const sinon = require('sinon');
+const { expect, assert } = require('chai');
+// const sinon = require('sinon');
 
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
-describe('getPaymentTokenFromAPI', (done) => {
-  it('promise response from API should resolve', () => {
+describe('getPaymentTokenFromAPI', () => {
+  it('promise response from API should resolve', (done) => {
     getPaymentTokenFromAPI(true)
-      .then((response) => {
-        expect(response).to.eql({ data: 'Successful response from the API' });
+      .then((result) => {
+        expect(result).to.eql({ data: 'Successful response from the API' });
         done();
       })
       .catch((error) => {
