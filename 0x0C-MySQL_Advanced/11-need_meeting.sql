32c6689f55c4f104d302c45f4c9adfd9f8fe1a2f
747-diego <725@holbertonschool.com>
2021-06-20 19:11:18 -0400

    [TESTING MEETING]

--- 0x0C-MySQL_Advanced/11-need_meeting.sql
+++ 0x0C-MySQL_Advanced/11-need_meeting.sql
@@ -1,4 +1,3 @@
 -- No table for a meeting
 -- SQL script that creates a view need_meeting
-CREATE VIEW need_meeting
-(IN user_id INT)
+CREATE VIEW need_meeting AS SELECT name FROM students WHERE score < 80 and (last_meeting IS NULL or last_meeting < date_sub(now(), interval 1 month));
