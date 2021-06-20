5751ba5be9200a471f0bcff7137be30c9a733105
747-diego <725@holbertonschool.com>
2021-06-20 12:58:41 -0400

    [TESTING PAYMENT]

--- 0x13-unittests_in_js/6-payment_token.test.js
+++ 0x13-unittests_in_js/6-payment_token.test.js
@@ -3,11 +3,12 @@ const { expect } = require('chai');
 
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
-describe('getPaymentTokenFromAPI', () => {
+describe('getPaymentTokenFromAPI', (done) => {
   it('promise response from API should resolve', () => {
     getPaymentTokenFromAPI(true)
       .then((response) => {
         expect(response).to.eql({ data: 'Successful response from the API' });
+        done();
       });
   });
 });
