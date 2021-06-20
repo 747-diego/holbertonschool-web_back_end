96c9d0581c5532fbd5c7610ac49d9c30ede8df30
747-diego <725@holbertonschool.com>
2021-06-20 19:24:46 -0400

    [TESTING SCORE FOR ALL]

--- 0x0C-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x0C-MySQL_Advanced/100-average_weighted_score.sql
@@ -6,4 +6,4 @@ BEGIN
 	SELECT sum(score * weight) / sum(weight * 100) * 100 FROM corrections LEFT JOIN projects ON corrections.project_id = projects.id WHERE user_id = corrections.user_id INTO @weighted_score;
 	UPDATE users SET average_score = @weighted_score WHERE user_id = users.id;
 END//
-DELIMITER ;
+DELIMITER;
