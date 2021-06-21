d9feeb55ca66a607da5c3fb1aa825b7096bbd56f
747-diego <725@holbertonschool.com>
2021-06-20 22:16:17 -0400

    [TESTING STUDENT BY ID IN CITIES]

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,16 @@
 export default function updateStudentGradeByCity(arrObj, city, newGrade) {
-  let response = arrObj.filter((stud) => stud.location === city);
+  let result = arrObj.filter((stud) => stud.location === city);
 
-  response = response.map((students) => {
-    // const students = students;
-    const score = newGrade.filter((city) => students.id === city.studentId);
-    if (score.length > 0) {
-      students.grade = score[0].grade;
+  result = result.map((studenta) => {
+    const student = studenta;
+    const res = newGrade.filter((x) => student.id === x.studentId);
+    if (res.length > 0) {
+      student.grade = res[0].grade;
     } else {
-      students.grade = 'N/A';
+      student.grade = 'N/A';
     }
-    return students;
+    return student;
   });
 
-  return response;
+  return result;
 }
