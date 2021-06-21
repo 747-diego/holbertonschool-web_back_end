3193e0c9c88cb18104793f3481f536d54fea5c6a
747-diego <725@holbertonschool.com>
2021-06-20 21:16:54 -0400

    [TESTING NEW VERSION]

--- 0x0D-NoSQL/102-log_stats.py
+++ 0x0D-NoSQL/102-log_stats.py
@@ -1,7 +1,37 @@
 #!/usr/bin/env python3
 """Log stats - new version."""
+from pymongo import MongoClient
 
+if __name__ == "__main__":
+    user = MongoClient()
+    docuLogs = user.logs
+    dependencies = docuLogs.nginx
+    all_docs = dependencies.find()
+    numberoFdocs = dependencies.count_documents({})
+    docuNum = dependencies.count_documents({"method": "GET"})
+    statusCode = dependencies.count_documents({"method": "POST"})
+    updatedLogs = dependencies.count_documents({"method": "PUT"})
+    needLogs = dependencies.count_documents({"method": "PATCH"})
+    removedLogs = dependencies.count_documents({"method": "DELETE"})
+    PKGs = dependencies.count_documents({"path": "/status"})
+    print(f"{numberoFdocs} logs\nMethods:")
+    print(f"\tmethod GET: {docuNum}")
+    print(f"\tmethod POST: {statusCode}")
+    print(f"\tmethod PUT: {updatedLogs}")
+    print(f"\tmethod PATCH: {needLogs}")
+    print(f"\tmethod DELETE: {removedLogs}")
+    print(f"{PKGs} status check")
+    print("IPs:")
 
-def log_stats(mongo_collection):
-    """New-version."""
-    pass
+    packagesID = [
+        {"$group": {
+            "_id": "$ip",
+            "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ]
+    adresses = dependencies.aggregate(packagesID)
+    for IPadress in adresses:
+        ip = IPadress.get("_id")
+        ipNum = IPadress.get("count")
+        print(f"\t{ip}: {ipNum}")
