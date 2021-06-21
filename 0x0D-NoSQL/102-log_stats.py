76b901a681f06f4d536b5ca027a9c60f78c68900
747-diego <725@holbertonschool.com>
2021-06-20 21:18:30 -0400

    [TESTING NEW VERSION]

--- 0x0D-NoSQL/102-log_stats.py
+++ 0x0D-NoSQL/102-log_stats.py
@@ -3,35 +3,35 @@
 from pymongo import MongoClient
 
 if __name__ == "__main__":
-    client = MongoClient()
-    db = client.logs
-    collection = db.nginx
-    all_docs = collection.find()
-    total_count = collection.count_documents({})
-    get_count = collection.count_documents({"method": "GET"})
-    post_count = collection.count_documents({"method": "POST"})
-    put_count = collection.count_documents({"method": "PUT"})
-    patch_count = collection.count_documents({"method": "PATCH"})
-    delete_count = collection.count_documents({"method": "DELETE"})
-    status_count = collection.count_documents({"path": "/status"})
-    print(f"{total_count} logs\nMethods:")
-    print(f"\tmethod GET: {get_count}")
-    print(f"\tmethod POST: {post_count}")
-    print(f"\tmethod PUT: {put_count}")
-    print(f"\tmethod PATCH: {patch_count}")
-    print(f"\tmethod DELETE: {delete_count}")
-    print(f"{status_count} status check")
+    user = MongoClient()
+    docuLogs = user.logs
+    dependencies = docuLogs.nginx
+    all_docs = dependencies.find()
+    numberoFdocs = dependencies.count_documents({})
+    docuNum = dependencies.count_documents({"method": "GET"})
+    statusCode = dependencies.count_documents({"method": "POST"})
+    updatedLogs = dependencies.count_documents({"method": "PUT"})
+    needLogs = dependencies.count_documents({"method": "PATCH"})
+    removedLogs = dependencies.count_documents({"method": "DELETE"})
+    PKGs = dependencies.count_documents({"path": "/status"})
+    print(f"{numberoFdocs} logs\nMethods:")
+    print(f"\tmethod GET: {docuNum}")
+    print(f"\tmethod POST: {statusCode}")
+    print(f"\tmethod PUT: {updatedLogs}")
+    print(f"\tmethod PATCH: {needLogs}")
+    print(f"\tmethod DELETE: {removedLogs}")
+    print(f"{PKGs} status check")
     print("IPs:")
 
-    pipeline = [
+    packagesID = [
         {"$group": {
             "_id": "$ip",
             "count": {"$sum": 1}}},
         {"$sort": {"count": -1}},
         {"$limit": 10}
     ]
-    ip_list = collection.aggregate(pipeline)
-    for ip in ip_list:
-        ip_addr = ip.get("_id")
-        ip_count = ip.get("count")
-        print(f"\t{ip_addr}: {ip_count}")
+    adresses = dependencies.aggregate(packagesID)
+    for IPadress in adresses:
+        ip = IPadress.get("_id")
+        ipNum = IPadress.get("count")
+        print(f"\t{ip}: {ipNum}")
