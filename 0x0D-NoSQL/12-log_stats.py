5379ef7e8d4e876bf0dde6ca3c584d4580491262
747-diego <725@holbertonschool.com>
2021-06-20 20:06:15 -0400

    [TESTING LOGGING STATS FROM NGINX]

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -1,29 +1,25 @@
 #!/usr/bin/env python3
-"""Python script that provides some stats about Nginx logs."""
+"""script that provides some stats about Nginx logs stored in MongoDB."""
 
 from pymongo import MongoClient
 
-mongodb = MongoClient()
-logs = mongodb.logs.nginx
+client = MongoClient()
+sessionTransfer = client.logs.nginx
 
-count = logs.count_documents({})
-get = logs.count_documents({"method": "GET"})
-post = logs.count_documents({"method": "POST"})
-put = logs.count_documents({"method": "PUT"})
-patch = logs.count_documents({"method": "PATCH"})
-delete = logs.count_documents({"method": "DELETE"})
-
-status = logs.count_documents({"method": "GET", "path": "/status"})
-documents = [get, post, put, patch, delete]
+numberOflogs = sessionTransfer.count_documents({})
+number = sessionTransfer.count_documents({"method": "GET"})
+statusCode = sessionTransfer.count_documents({"method": "POST"})
+updatedLogs = sessionTransfer.count_documents({"method": "PUT"})
+dependencies = sessionTransfer.count_documents({"method": "PATCH"})
+removedLogs = sessionTransfer.count_documents({"method": "DELETE"})
+PKGs = sessionTransfer.count_documents({"method": "GET", "path": "/status"})
 
 if __name__ == "__main__":
-    print(f"{count} logs")
+    print(f"{numberOflogs} logs")
     print("Methods:")
-
-    for method in documents:
-        print(f"\tmethod GET: {get}")
-        print(f"\tmethod POST: {post}")
-        print(f"\tmethod PUT: {put}")
-        print(f"\tmethod PATCH: {patch}")
-        print(f"\tmethod DELETE: {delete}")
-    print(f"{status} status check")
+    print(f"\tmethod GET: {number}")
+    print(f"\tmethod POST: {statusCode}")
+    print(f"\tmethod PUT: {updatedLogs}")
+    print(f"\tmethod PATCH: {dependencies}")
+    print(f"\tmethod DELETE: {removedLogs}")
+    print(f"{PKGs} status check")
