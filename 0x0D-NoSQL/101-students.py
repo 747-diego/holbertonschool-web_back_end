8d0757bdd92efdd1f4f636304f5f90e07962ce82
747-diego <725@holbertonschool.com>
2021-06-20 21:10:17 -0400

    [TOP STUDENTS]

--- 0x0D-NoSQL/101-students.py
+++ 0x0D-NoSQL/101-students.py
@@ -4,12 +4,12 @@
 
 def top_students(mongo_collection):
     """Python function that returns all students sorted by average score."""
-    students = [x for x in mongo_collection.find()]
-    for student in students:
-        scores = [x.get('score', 0) for x in student.get('topics', [])]
-        avg = 0
-        for score in scores:
-            avg = avg + score
-        avg = avg / len(scores)
-        student["averageScore"] = avg
-    return sorted(students, key=lambda z: z["averageScore"], reverse=True)
+    topStudents = [x for x in mongo_collection.find()]
+    for student in topStudents:
+        average = [x.get('score', 0) for x in student.get('topics', [])]
+        points = 0
+        for topScore in average:
+            points = points + topScore
+        points = points / len(average)
+        student["averageScore"] = points
+    return sorted(topStudents, key=lambda z: z["averageScore"], reverse=True)
