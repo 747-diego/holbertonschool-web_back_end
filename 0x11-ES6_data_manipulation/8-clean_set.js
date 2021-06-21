ab13dcd083394b3d47869b555931a704ec43bdb5
747-diego <725@holbertonschool.com>
2021-06-20 22:34:29 -0400

    [TESTING THE CLEAN SHEET]

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,8 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string') return '';
+  if (!startString || typeof startString !== 'string')
+    return '';
   const list = Array.from(set).filter(
-    (firstHalf) => (firstHalf ? firstHalf.includes(startString) : '')
+    (firstHalf) => (firstHalf ? firstHalf.includes(startString) : ''),
   ).map((secondHalf) => secondHalf.slice(startString.length)).join('-');
   return (list);
 }
