a64aaaf964508dd59b9ab0d2535d1072008a7415
747-diego <725@holbertonschool.com>
2021-06-20 20:22:36 -0400

    [TESTING LOGGING STATS FROM NGINX]

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -4,14 +4,14 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    collection = client.logs.nginx
-    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    localhost = MongoClient('mongodb://127.0.0.1:27017')
+    dependencies = localhost.logs.nginx
+    docuLogs = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-    print('{} logs'.format(collection.count_documents({})))
+    print('{} logs'.format(dependencies.count_documents({})))
     print("Methods:")
-    for i in method:
+    for packages in docuLogs:
         print('\tmethod {}: {}'.format(
-            i, collection.count_documents({'method': i})))
+            packages, dependencies.count_documents({'method': packages})))
     print('{} status check'.format(
-        collection.count_documents({'path': '/status'})))
+        dependencies.count_documents({'path': '/status'})))
